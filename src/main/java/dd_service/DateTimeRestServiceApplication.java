7da5a680990c3cabf8cf67ae49669771e78bd953
petz1209 <petz.leitner@gmx.at>
2022-06-22 18:17:05 +0200

    Initial commit

---
+++ src/main/java/dd_service/DateTimeRestServiceApplication.java
@@ -0,0 +1,13 @@
+package dd_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateTimeRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateTimeRestServiceApplication.class, args);
+	}
+
+}
